4c7cdd165f47b0464ae1352469f15f9c2c9a7513
Abena Ofori <abenaboama@gmail.com>
2016-01-08 19:36:46 -0500

    Checkpoint 22 menu: completed

--- models/address_book.rb
+++ models/address_book.rb
@@ -7,19 +7,16 @@ class AddressBook
     @entries = []
   end
 
-
-
   def add_entry(name, phone, email)
-    # #9
     index = 0
     @entries.each do |entry|
-      # #10
+
       if name < entry.name
         break
       end
       index += 1
     end
-    # #11
+
     @entries.insert(index, Entry.new(name, phone, email))
   end
 end
--- spec/address_book_spec.rb
+++ spec/address_book_spec.rb
@@ -36,5 +36,22 @@ RSpec.describe AddressBook do
       expect(new_entry.phone_number).to eq '010.012.1815'
       expect(new_entry.email).to eq 'augusta.king@lovelace.com'
     end
-  end  
+  end
+
+  context "#remove_entry" do
+    it "removes an entry using the name, phone_number, and email address" do
+      book = AddressBook.new
+      book.add_entry("Ada Lovelace", "010.012.2345", "ama.love@good.com")
+
+      name = "Ada Lovelace"
+      phone_number = "010.012.1815"
+      email_address = "augusta.king@lavelace.com"
+      book.add_entry(name, phone_number, email_address)
+
+      expect(book.entries.size).to eq 2
+      book.remove_entry(name, phone_number, email_address)
+      expect(book.entries.size).to eq 1
+      expect(book.entries.name).to eq ("Ada Lovelace")
+   end
+ end
 end
